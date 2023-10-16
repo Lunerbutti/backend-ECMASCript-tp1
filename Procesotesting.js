ced37fd44fed44cdef7238179bd73694d6af5b72
Lu Nerbutti <lunerbutti@gmail.com>
2023-10-16 20:31:53 -0300

    creacion del class proceso de testing

---
+++ Procesotesting.js
@@ -0,0 +1,22 @@
+// Trabajo práctico: Proceso de Testing 
+
+class ProductManager{
+    constructor(){
+        this.products=[]
+    } 
+
+    getProduct(){
+        return this.products
+    }
+    addProduct(){
+    }
+// Cada Producto debe tener 
+// title (nombre del producto)
+// description (descripción del producto)
+// price (precio)
+// thumbnail (ruta de imagen)
+// code (código identificador)
+// stock (número de piezas disponibles)
+
+    
+}
