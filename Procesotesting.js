f81fd5485653714cfd498aae39d41a920f0e9f45
Lu Nerbutti <lunerbutti@gmail.com>
2023-10-16 21:17:11 -0300

    mandatory field

--- Procesotesting.js
+++ Procesotesting.js
@@ -1,22 +1,35 @@
 // Trabajo práctico: Proceso de Testing 
 
 class ProductManager{
+    // creacion del constructor 
     constructor(){
         this.products=[]
     } 
-
+    // traer todos los productos
     getProduct(){
         return this.products
     }
-    addProduct(){
+    // Agregar productos
+    addProduct(title, description, price,thumbnail,code,stock){
+        // creacion de id
+        let id=1
+        if(this.products.length>0){
+            id=this.products[this.products.length-1].id+1
+        // nuevo producto
+        let newProduct={
+            id,
+            title:<required/>,
+            description:<required/>,
+            price:<required/>,
+            thumbnail:<required/>,
+            code:<required/>,
+            stock:<required/>,
+        }    
+        this.products.push(newProduct)
+        }
+
     }
-// Cada Producto debe tener 
-// title (nombre del producto)
-// description (descripción del producto)
-// price (precio)
-// thumbnail (ruta de imagen)
-// code (código identificador)
-// stock (número de piezas disponibles)
+
 
     
 }
