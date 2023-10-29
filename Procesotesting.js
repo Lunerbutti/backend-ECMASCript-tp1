c2ae2c3a2dc82906b385a6423a4184df3a535954
Lu Nerbutti <lunerbutti@gmail.com>
2023-10-29 19:42:13 -0300

    actualizacion delete product y update product

--- Procesotesting.js
+++ Procesotesting.js
@@ -1,5 +1,6 @@
 // Trabajo práctico: Proceso de Testing 
 const fs=require('fs')
+const { title } = require('process')
 
 class ProductManager{
     // creacion del constructor 
@@ -24,7 +25,7 @@ class ProductManager{
         if(this.products.length>0){
             id=this.products[this.products.length-1].id+1}
         // nuevo producto
-        
+
         let newProduct={
             id,
             title,
@@ -37,10 +38,10 @@ class ProductManager{
         if (!id|| !title|| !description|| !price|| !thumbnail|| !code|| !stock === undefined){
             console.log("Error! - You must fill all the fields")}
         this.products.some((product)=>product.code===newProduct.code)?
-                console.log("the caode already exist, please try again"):          
+                console.log("the code already exist, please try again"):          
         this.products.push(newProduct)
         // escritura en el archivo 
-        fs.writeFileSync(this.path, JSON.stringify(newProduct, null, "\t"))    
+        fs.writeFileSync(this.path, JSON.stringify(this.products, null, "\t"))    
     }            
         // Llamar producto por ID
     getProductbyID(id){
@@ -53,19 +54,43 @@ class ProductManager{
         return this.products[indiceID]; 
     }  
     // Update product
-    updateProduct(id){
-        
-    }
+    updateProduct(productId, fieldsToUpdate) {
+
+            try {
+          
+              const products = this.getProduct();
+          
+              const productIndex = products.findIndex(product => product.id === Number(productId));
+             if (productIndex === -1) {
+                return console.error(`Product with ID ${productId} not found`);
+                }
+              const productToUpdate = products[productIndex];
+              const updatedProduct = { ...productToUpdate, ...fieldsToUpdate };
+              products[productIndex] = updatedProduct;
+          
+             fs.writeFileSync(this.path, JSON.stringify(this.products, null, "\t"));
+                return {
+                message: "Product updated!",
+                product: updatedProduct
+                };
+            } catch (error) {
+              console.error("Error updating product:", error);
+              throw error; // You may want to throw the error for better error handling in the calling code.
+            }
+            }  
+ 
     // Borrar producto
     deleteProduct(id){
         let indiceID=this.products.findIndex(product=>product.id===id)
-        let deleted = false;
+        // let deleted = false;
         if(indiceID=== -1){
             console.log(`the id request dosent exist `)
             return
         }else{
-         this.products[indiceID] = deleted = true;
-         console.log('the product is deleted')
+        //  this.products[indiceID] = deleted = true;
+         this.products.splice(indiceID,1)
+         console.log(`the product is deleted` ) 
+        return this.getProduct()
         }
         
     }
@@ -77,12 +102,13 @@ let pm=new ProductManager('./Archivo/productList.json')
 pm.addProduct('pantalonJS','pantalon de jean largo','500','sin imagen', '5000', '10')
 pm.addProduct('Remera MC','Remera mangas cortas','700','sin imagen', '5323', "7")
 pm.addProduct('Musculosa','musculosa amarilla','700','sin imagen', '5501', "20")
-pm.addProduct('Musculosa','musculosa amarilla','700','sin imagen', '5501', "20")
 console.log(pm.getProduct());
 console.log(pm.getProductbyID(20));
 console.log(pm.getProductbyID(2));
 console.log(pm.deleteProduct(1));
+pm.addProduct('Musculosa','musculosa amarilla','700','sin imagen', '5501', "20")
 console.log(pm.getProduct());
+console.log(pm.updateProduct(3, {title:'top', description:'pendiente de actualizar'}))
 
 
 
